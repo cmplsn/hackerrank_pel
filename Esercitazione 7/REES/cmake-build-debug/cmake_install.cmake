06ba337df78ed8d53550c2fa667feb33acaa3fb8
alessandrocampagnaro <cmplsn97@gmail.com>
2022-06-04 12:48:08 +0200

    aggiunte 2 cartelle

--- Esercitazione 7/REES/cmake-build-debug/cmake_install.cmake
+++ Esercitazione 7/REES/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/alessandrocampagnaro/Desktop/es_hackerrank/Esercitazione 7/REES
+# Install script for directory: /Users/alessandrocampagnaro/Desktop/hackerrank_pel/Esercitazione 7/REES
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/alessandrocampagnaro/Desktop/es_hackerrank/Esercitazione 7/REES/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/alessandrocampagnaro/Desktop/hackerrank_pel/Esercitazione 7/REES/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
